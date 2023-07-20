cc05cdb104b82580f6371f073b2b0a6710a23283
zox1r <qarshiyevzoxir@gmail.com>
2023-07-20 11:40:15 +0200

    basicfunc11.dart

--- basicfunc11.dart
+++ basicfunc11.dart
@@ -1,4 +1,11 @@
 // Create a function called func.
 // Create function argument a.
 // Return the absolute value of a.
-void main() {}
+int func(int a){
+    return a;
+}
+
+
+void main() {
+    print(func(12));
+}
