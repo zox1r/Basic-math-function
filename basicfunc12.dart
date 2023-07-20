0b51797fcdd02e26755c1f26cf267f841b0e3787
zox1r <qarshiyevzoxir@gmail.com>
2023-07-20 11:51:22 +0200

    basicfunc12.dart

--- basicfunc12.dart
+++ basicfunc12.dart
@@ -2,4 +2,11 @@
 // Create function argument a.
 // Return the value of a.
 // Round the result.
-void main() {}
+num func(num a){
+ return a.round();
+}
+
+
+void main() {
+    print(func(1.6));
+}
