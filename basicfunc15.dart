cba6e8b9c36d3bd8ed1ad959c1e500b2b8ff9af3
zox1r <qarshiyevzoxir@gmail.com>
2023-07-21 14:39:14 +0200

    basicfunc15.dart

--- basicfunc15.dart
+++ basicfunc15.dart
@@ -1,4 +1,11 @@
 // Create a function called main.
 // Create function arguments a and b.
 // Return the modulus of a and b.
-void main() {}
+int func(int num1, int num2){
+return num1 ~/ num2;
+}
+
+
+void main() {
+    print(func(4, 2));
+}
