e222013af94137cf367c1a253f7a29464795cc06
zox1r <qarshiyevzoxir@gmail.com>
2023-07-22 07:54:30 +0200

    basicfunc19.dart

--- basicfunc19.dart
+++ basicfunc19.dart
@@ -1,4 +1,10 @@
 // Create a function called main.
 // Create function arguments a and b.
 // Return the absolute value of the difference between a and b.
-void main() {}
+int func(int a, int b){
+    return a - b;
+}
+
+void main() {
+    print(func(10, 7));
+}
