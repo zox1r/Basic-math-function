2a47088d45a83b6d659b0f982992397b355338a3
zox1r <qarshiyevzoxir@gmail.com>
2023-07-22 07:53:33 +0200

    basicfunc17.dart

--- basicfunc17.dart
+++ basicfunc17.dart
@@ -3,4 +3,11 @@ import 'dart:math';
 // Create a function called main.
 // Create function arguments a.
 // Assign the value pi to the variable "a" and return.
-void main() {}
+int func(int a){
+    return a;
+}
+
+
+void main() {
+ print(func(pi));
+}
