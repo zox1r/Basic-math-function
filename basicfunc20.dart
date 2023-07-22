90b1892c9e1d8639567bb026bb69071ad31c35f2
zox1r <qarshiyevzoxir@gmail.com>
2023-07-22 07:55:00 +0200

    basicfunc20.dart

--- basicfunc20.dart
+++ basicfunc20.dart
@@ -1,4 +1,11 @@
 // Create a function called main.
 // Create function arguments a and b.
 // Return the multiplication of a and b.
-void main() {}
+num func(num a, num b){
+    return a * b;
+}
+
+
+void main() {
+    print(func(2, 3));
+}
